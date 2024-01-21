8ae9e22b43e95beb48c55622b79993b3dbd13bec
martin.calsina <martincalsina05@gmail.com>
2024-01-21 17:31:32 -0300

    template del login y del signup hechos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}, 
-  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)}
+  {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
+  {path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule)}
 ];
 
 @NgModule({
--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SignupComponent
+  ],
   imports: [
     CommonModule,
     SignupRoutingModule
